12d4135a4572935cbad55f0168d32ea7cfc5e4d4
Nicklas <nicklascschmidt@Nicklass-MacBook-Pro.local>
2019-01-29 00:04:15 -0800

    form input complete

---
+++ client/src/components/questionForm/QuestionForm.js
@@ -0,0 +1,59 @@
+
+import React from "react";
+import moment from 'moment';
+import { Button, Form, FormGroup, Label, FormText } from 'reactstrap';
+
+class QuestionForm extends React.Component {
+  constructor(props) {
+      super(props);
+
+      this.state = {
+        min: 1950,
+        max: parseInt(moment().format('YYYY')),
+        formInputRange: 0,
+      };
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      formInputRange: Math.round((this.state.max + this.state.min) / 2)
+    });
+  };
+
+  handleChange = event => {
+    let { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    // let { mailTo, emailName, emailDomain, submitterSubject, submitterMessage } = this.state;
+  }
+
+  handleSliderChange = (event) => {
+    let { id, value } = event.target;
+    this.setState({
+      [id]: parseInt(value)
+    });
+  }
+
+
+  render() {
+    return (
+      <Form>
+        <FormGroup>
+          <Label for="formInputRange">Your Guess: {this.state.formInputRange}</Label>
+          <input type='range' id="formInputRange" min={this.state.min} max={this.state.max} value={this.state.formInputRange} onChange={event => this.handleSliderChange(event)}/>
+        </FormGroup>
+        <FormText color="muted">
+          *submit will open external default mail client
+        </FormText>
+      </Form>
+    );
+  }
+}
+
+export default QuestionForm;
