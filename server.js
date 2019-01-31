903912578b04935c96999c53a156159c5407ebd9
Nicklas <nicklascschmidt@Nicklass-MacBook-Pro.local>
2019-01-31 14:53:50 -0800

    heroku

--- server.js
+++ server.js
@@ -5,21 +5,16 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+require("./routes/html-routes")(app);
+require("./routes/scrape-routes")(app);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Static directory
-// app.use('/static', express.static(path.join(__dirname, 'client/build/static')));
-app.use(express.static(path.join(__dirname, 'client/build')));
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  // app.use('/static', express.static(path.join(__dirname, 'client/build/static')));
   app.use(express.static(path.join(__dirname, 'client/build')));
 }
-
-require("./routes/html-routes")(app);
-require("./routes/scrape-routes")(app);
 
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
